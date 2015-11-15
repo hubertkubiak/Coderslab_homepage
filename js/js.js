3d8bb2a9d28cbf6863cbf4a53c59712885f35b09
hubertkubiak <hubkubiak@gmail.com>
2015-11-15 20:23:02 +0100

    tidying up the code

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 /**
  * Created by Hubert on 2015-10-29.
  */
-$(function(){
+$(function () {
 
-    var Application = function(){
-        function animsite () {
+    var Application = function () {
+        function animsite() {
             $(".animsition").animsition({
                 inClass: 'fade-in-down',
                 outClass: 'fade-out-up',
@@ -17,7 +17,7 @@ $(function(){
                 timeout: false,
                 timeoutCountdown: 5000,
                 onLoadEvent: true,
-                browser: [ 'animation-duration', '-webkit-animation-duration']
+                browser: ['animation-duration', '-webkit-animation-duration']
             });
             $(".animsition2").animsition({
                 inClass: 'fade-in-left',
@@ -31,121 +31,125 @@ $(function(){
                 timeout: false,
                 timeoutCountdown: 5000,
                 onLoadEvent: true,
-                browser: [ 'animation-duration', '-webkit-animation-duration']
+                browser: ['animation-duration', '-webkit-animation-duration']
             });
         }
-        function hideHeading () {
+
+        function hideHeading() {
             var menu = $(".navbar");
             var header = $("header");
 
             $(window).scroll(function (event) {
-                if (menu.offset().top > header.offset().top+50) {
+                if (menu.offset().top > header.offset().top + 50) {
                     header.addClass("faded", 400);
                 }
-                if (menu.offset().top <= header.offset().top+50) {
+                if (menu.offset().top <= header.offset().top + 50) {
                     header.removeClass("faded", 400);
                 }
             });
         }
-        function hamburger(){
+
+        function hamburger() {
             var ham = $(".hamburger");
             var menuhamburger = $(".menu-hamburger");
 
-            ham.on("click", function(event){
+            ham.on("click", function (event) {
                 menuhamburger.toggleClass("unwind", 500);
             });
             var hamlinks = $(".ham a");
-            hamlinks.on("click", function(event){
+            hamlinks.on("click", function (event) {
                 menuhamburger.addClass("unwind", 500);
             })
         }
-        function highlightOption(){
-                var optionOmnie = $(".omnie-option");
 
-                optionOmnie.on("mouseenter", function(event){
-                    optionOmnie.pulse(
-                        {
-                            backgroundColor: '#b1b1b1'
-                        },
-                        {
-                            returnDelay : 60000,
+        function highlightOption() {
+            var optionOmnie = $(".omnie-option");
 
-                            pulses      : 1
-                        }
-                    );
-                });
+            optionOmnie.on("mouseenter", function (event) {
+                optionOmnie.pulse(
+                    {
+                        backgroundColor: '#b1b1b1'
+                    },
+                    {
+                        returnDelay: 60000,
 
-                optionOmnie.on("mouseleave", function(event){
-                    optionOmnie.pulse(
-                        {
-                        backgroundColor:  '#EFEDEA'
-                        },
-                        {
-                        returnDelay : 30000,
+                        pulses: 1
+                    }
+                );
+            });
 
-                        pulses      : 1
-                        }
-                    );
-                });
+            optionOmnie.on("mouseleave", function (event) {
+                optionOmnie.pulse(
+                    {
+                        backgroundColor: '#EFEDEA'
+                    },
+                    {
+                        returnDelay: 30000,
+
+                        pulses: 1
+                    }
+                );
+            });
 
             var optionPortfolio = $(".portfolio-option");
 
-            optionPortfolio.on("mouseenter", function(event){
+            optionPortfolio.on("mouseenter", function (event) {
                 optionPortfolio.pulse(
                     {
                         backgroundColor: '#b1b1b1'
                     },
                     {
-                        returnDelay : 60000,
+                        returnDelay: 60000,
 
-                        pulses      : 1
+                        pulses: 1
                     }
                 );
             });
 
-            optionPortfolio.on("mouseleave", function(event){
+            optionPortfolio.on("mouseleave", function (event) {
                 optionPortfolio.pulse(
                     {
-                        backgroundColor:  '#EFEDEA'
+                        backgroundColor: '#EFEDEA'
                     },
                     {
-                        returnDelay : 30000,
+                        returnDelay: 30000,
 
-                        pulses      : 1
+                        pulses: 1
                     }
                 );
             });
 
             var optionKontakt = $(".contakt-option");
 
-            optionKontakt.on("mouseenter", function(event){
+            optionKontakt.on("mouseenter", function (event) {
                 optionKontakt.pulse(
                     {
                         backgroundColor: '#b1b1b1'
                     },
                     {
-                        returnDelay : 60000,
+                        returnDelay: 60000,
 
-                        pulses      : 1
+                        pulses: 1
                     }
                 );
             });
 
-            optionKontakt.on("mouseleave", function(event){
+            optionKontakt.on("mouseleave", function (event) {
                 optionKontakt.pulse(
                     {
-                        backgroundColor:  '#EFEDEA'
+                        backgroundColor: '#EFEDEA'
                     },
                     {
-                        returnDelay : 30000,
+                        returnDelay: 30000,
 
-                        pulses      : 1
+                        pulses: 1
                     }
                 );
             })
 
         }
-        function hideOutElements () {
+
+        function hideOutElements() {
             var menu = $(".navbar");
             var firstSection = $("#omnie");
             var oMnie = $(".omnie-container");
@@ -155,57 +159,59 @@ $(function(){
             var contactData = $('.contactdata');
 
             $(window).scroll(function (event) {
-                if (menu.offset().top > firstSection.offset().top-100) {
+                if (menu.offset().top > firstSection.offset().top - 100) {
                     oMnie.pulse(
                         {
                             color: "white"
                         },
                         {
-                            returnDelay : 1000000,
-                            pulses      : 1
+                            returnDelay: 1000000,
+                            pulses: 1
                         }
                     );
                 }
-                if (menu.offset().top > portfolio.offset().top-100) {
+                if (menu.offset().top > portfolio.offset().top - 100) {
                     portfolioH1.pulse(
                         {
                             color: "white"
                         },
                         {
-                            returnDelay : 1000000,
-                            pulses      : 1
+                            returnDelay: 1000000,
+                            pulses: 1
                         }
                     );
                 }
-                if (menu.offset().top > contact.offset().top-100) {
+                if (menu.offset().top > contact.offset().top - 100) {
                     contactData.pulse(
                         {
                             color: "white"
                         },
                         {
-                            returnDelay : 1000000,
-                            pulses      : 1
+                            returnDelay: 1000000,
+                            pulses: 1
                         }
                     );
                 }
             });
         }
-        function scrollPage(){
+
+        function scrollPage() {
             var navigationOpt = $(".menu1 a");
-            navigationOpt.on("click", function(event){
+            navigationOpt.on("click", function (event) {
                 var hrefs = $(this).attr("href");
                 $('html,body').animate({
-                    scrollTop:$(hrefs).offset().top
+                    scrollTop: $(hrefs).offset().top
                 }, 400);
             });
             var circlesOpt = $(".option a");
-            circlesOpt.on("click", function(event){
+            circlesOpt.on("click", function (event) {
                 var hrefs = $(this).attr("href");
                 $('html,body').animate({
-                    scrollTop:$(hrefs).offset().top
+                    scrollTop: $(hrefs).offset().top
                 }, 400);
             });
         }
+
         return {
             hideHeading: hideHeading,
             hamburger: hamburger,
